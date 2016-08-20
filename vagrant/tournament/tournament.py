1db792b5d9215d771c734e4950669e668e7a6633
karlhyang <karlhyang@gmail.com>
2016-08-20 01:16:27 -0700

    Add files via upload
    project submission

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,23 +13,41 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM matches;")
+    conn.commit()
+    conn.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM players;")
+    conn.commit()
+    conn.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute(
+        "SELECT COUNT(*) from players;")
+    val = c.fetchone()[0]
+    return val
+    conn.close()
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
     The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
-  
-    Args:
+    should be handled by your SQL database schema, not in your Python code.)"""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO players (name) values (%s);",(name,))
+    conn.commit()
+    conn.close() 
+    """ Args:
       name: the player's full name (need not be unique).
     """
 
@@ -38,8 +56,14 @@ def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
     The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
-
+    tied for first place if there is currently a tie."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT players.id, players.name, (select count(*) from matches where winner = players.id) as wins, (select count(*) from matches where winner = players.id or loser = players.id) as matches_played from players order by wins;")
+    result = c.fetchall()
+    return result
+    conn.close() 
+    """
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
         id: the player's unique id (assigned by the database)
@@ -50,8 +74,13 @@ def playerStandings():
 
 
 def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
-
+    """Records the outcome of a single match between two players."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO matches (winner,loser) values (%s, %s);",(winner,loser))
+    conn.commit()
+    conn.close()
+    """
     Args:
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
@@ -64,8 +93,30 @@ def swissPairings():
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
+    to him or her in the standings."""
+
+    standings = playerStandings()
+    thepairings = [] 
+    y = 0
+    while y < len(standings): #iterating until we have connected every pair
+        pairing = []
+        id1 = standings[0+y][0] # Pulling out the relevant information we want
+        name1 = standings [0+y][1]
+        id2 = standings[1+y][0]
+        name2 = standings[1+y][1]
+        pairing.append(id1) #we add to the tuple the info we want
+        pairing.append(name1)
+        pairing.append(id2)
+        pairing.append(name2)
+        thepairings.append(pairing) #
+        y += 2 #iterating through pairs
+    return thepairings
+
+
+
+
+
+    """
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+\c tournament;
 
+
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS players;
+create table players
+	(id 		SERIAL CONSTRAINT firstkey PRIMARY KEY,
+	 name 		varchar(40) not null
+	 )
+;
+create table matches
+	(id 		SERIAL UNIQUE,
+	 winner		integer references players (id),
+	 loser 		integer references players (id)
+	 )
+;
